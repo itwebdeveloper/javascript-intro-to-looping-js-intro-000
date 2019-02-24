419bbfa193baa07ba94d0ae54152119f885dca64
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 22:23:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ function incrementVariable(i) {
 
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
-    k = i 
+    k = i
     array[i] = `"I am ${k} strange loop${k === 0 ? '' : 's'}."`;
   }
   return array;
